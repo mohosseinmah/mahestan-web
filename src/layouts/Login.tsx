c4da54f22dc75a4ccbe94df1f327fadfe2aab04b
MohammadHossein <mohomah06@gmail.com>
2019-12-22 20:47:33 +0330

    - Add Login layout and its css, font and image files

---
+++ src/layouts/Login.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../assets/css/vendors.min.css";
+import "../assets/css/style-rtl.min.css";
+import "../assets/css/materialize.min.css";
+import "../assets/css/style.min.css";
+import "../assets/css/login.css";
+import "../assets/css/custom.css";
+
+class Login extends React.Component {
+
+    render() {
+        return <h1 className="login-bg">Login</h1>;
+    }
+}
+
+export default Login;
