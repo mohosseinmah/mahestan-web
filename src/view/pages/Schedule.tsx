359ad51d85218dba7e7805ef4210a53402e7e92f
MohammadHossein <mohomah06@gmail.com>
2020-01-02 16:09:28 +0330

    - Use css classes instead of inline style in Schedule page - Fix bug of afternoon offsets

--- src/view/pages/Schedule.tsx
+++ src/view/pages/Schedule.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties} from "react";
+import React from "react";
 import Breadcrumb from "../components/Breadcrumb";
 import Page from "../components/Page";
 import Row from "../components/Row";
@@ -10,23 +10,9 @@ import Preloader from "../components/Preloader";
 class Schedule extends React.Component {
     private readonly weekDays: any[][];
 
-    private readonly border: string;
-    private readonly rowStyle: CSSProperties;
-    private readonly columnStyle: CSSProperties;
-
     constructor(props: any) {
         super(props);
         this.weekDays = [[], [], [], [], []];
-
-        this.border = "0.5px solid";
-        this.rowStyle = {borderTop: this.border, borderBottom: this.border};
-        this.columnStyle = {
-            borderRight: this.border,
-            borderLeft: this.border,
-            height: "70px",
-            overflow: "hidden"
-        };
-
         findSchedule(this.setCourses);
     }
 
@@ -37,36 +23,36 @@ class Schedule extends React.Component {
                 <Page>
                     {this.isScheduleReady() ?
                         <Row>
-                            <Col className="s12 center-align" style={this.rowStyle}>
-                                <Col className="s1 offset-s2 grey lighten-3" style={this.columnStyle}>08:00 تا
+                            <Col className="s12 center-align schedule-row">
+                                <Col className="s1 offset-s2 grey lighten-3 schedule-column">08:00 تا
                                     09:00</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>09:00 تا 10:00</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>10:00 تا 11:00</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>11:00 تا 12:00</Col>
-                                <Col className="s1 offset-s1 grey lighten-3" style={this.columnStyle}>13:30 تا
+                                <Col className="s1 grey lighten-3 schedule-column">09:00 تا 10:00</Col>
+                                <Col className="s1 grey lighten-3 schedule-column">10:00 تا 11:00</Col>
+                                <Col className="s1 grey lighten-3 schedule-column">11:00 تا 12:00</Col>
+                                <Col className="s1 offset-s1 grey lighten-3 schedule-column">13:30 تا
                                     14:30</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>14:30 تا 15:30</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>15:30 تا 16:30</Col>
-                                <Col className="s1 grey lighten-3" style={this.columnStyle}>16:30 تا 17:30</Col>
+                                <Col className="s1 grey lighten-3 schedule-column">14:30 تا 15:30</Col>
+                                <Col className="s1 grey lighten-3 schedule-column">15:30 تا 16:30</Col>
+                                <Col className="s1 grey lighten-3 schedule-column">16:30 تا 17:30</Col>
                             </Col>
-                            <Col className="s12" style={this.rowStyle}>
-                                <Col className="s2 grey lighten-3" style={this.columnStyle}>شنبه</Col>
+                            <Col className="s12 schedule-row">
+                                <Col className="s2 grey lighten-3 schedule-column">شنبه</Col>
                                 {this.weekDays[0]}
                             </Col>
-                            <Col className="s12" style={this.rowStyle}>
-                                <Col className="s2 grey lighten-3" style={this.columnStyle}>یکشنبه</Col>
+                            <Col className="s12 schedule-row">
+                                <Col className="s2 grey lighten-3 schedule-column">یکشنبه</Col>
                                 {this.weekDays[1]}
                             </Col>
-                            <Col className="s12" style={this.rowStyle}>
-                                <Col className="s2 grey lighten-3" style={this.columnStyle}>دوشنبه</Col>
+                            <Col className="s12 schedule-row">
+                                <Col className="s2 grey lighten-3 schedule-column">دوشنبه</Col>
                                 {this.weekDays[2]}
                             </Col>
-                            <Col className="s12" style={this.rowStyle}>
-                                <Col className="s2 grey lighten-3" style={this.columnStyle}>سه شنبه</Col>
+                            <Col className="s12 schedule-row">
+                                <Col className="s2 grey lighten-3 schedule-column">سه شنبه</Col>
                                 {this.weekDays[3]}
                             </Col>
-                            <Col className="s12" style={this.rowStyle}>
-                                <Col className="s2 grey lighten-3" style={this.columnStyle}>چهارشنبه</Col>
+                            <Col className="s12 schedule-row">
+                                <Col className="s2 grey lighten-3 schedule-column">چهارشنبه</Col>
                                 {this.weekDays[4]}
                             </Col>
                         </Row>
@@ -104,11 +90,11 @@ class Schedule extends React.Component {
                 duration = session.endingTime - session.startingTime;
                 offset = session.startingTime - lastEndingTime;
                 if (lastEndingTime < 13 && session.startingTime > 13) {
-                    offset = Math.floor(offset) + 1;
+                    offset = Math.floor(offset);
                 }
                 lastEndingTime = session.endingTime;
                 this.weekDays[session.weekDay].push(
-                    <Col className={`s${duration} offset-s${offset}`} style={this.columnStyle}>{session.name}</Col>
+                    <Col className={`s${duration} offset-s${offset} schedule-column`}>{session.name}</Col>
                 );
             }
         }
